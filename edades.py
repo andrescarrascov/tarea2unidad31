782b4125ae083a8e35b9541315ff84654446425a
ANDRES CARRASCO <andres-carrasco-v@hotmail.com>
2024-07-20 03:17:15 -0500

    cambio 1

---
+++ edades.py
@@ -0,0 +1,14 @@
+
+    # carrasco velaso edison andres
+aoc=286
+aoc += 520
+print("la venta de las dos periodos calculados antes fue: $",aoc)
+
+aoc *= 2
+print("la ventas proyectadas para cuatro meses posteriores será: $",aoc)
+
+aoc -= 128
+print("la venta menos el costo de venta será: $",aoc)
+
+aoc /= 4
+print("la utilidad promedio mensual proyectada será : $",aoc)
